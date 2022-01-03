340351d8ba75620fb457d6a8ed1929c7a02ab883
diegoolguinw <74310240+diegoolguinw@users.noreply.github.com>
2022-01-03 10:38:47 -0300

    Se calcula N dentro de omori_RK4, se incluye hint

--- ejercicios_tn2010.py
+++ ejercicios_tn2010.py
@@ -11,7 +11,8 @@ def fd(kM, p, t):
 def yMa(kM, p, c, t):
     return kM*(np.log(np.exp(p*t)-1)-p*t) + c
 
-def omori_RK4(kM,p,c,t0,T,h,N,f):
+def omori_RK4(kM,p,c,t0,T,h,f):
+    N = int((T-t0)/h)
     t = np.linspace(t0,T,N)
     y_RK4 = np.zeros(N)
     y_RK4[0] = yMa(kM, p, c, t0)
@@ -22,3 +23,8 @@ def omori_RK4(kM,p,c,t0,T,h,N,f):
         g4 = f(kM, p,t[i+1])
         y_RK4[i+1] = y_RK4[i] + h/6*(g1+2*g2+2*g3+g4)
     return y_RK4
+
+def hint_1():
+    print("Recuerde que debe imponer la misma condición inicial que las partes anteriores.")
+    print("El método de RK4 se basa en calcular g1, g2, g3 y g4, luego calcular el siguiente elemento de la solución")
+    print("con la recurrencia conocida.")
